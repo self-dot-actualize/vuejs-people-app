14a4f0fd64290572a64e96e88ffb891e91e00297
Peter Jang <peter.x.jang@gmail.com>
2017-04-24 20:07:44 -0500

    Add new person using v-on:click

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -8,11 +8,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
         {name: "Alice", bio: "Tattooed letterpress gluten-free ugh", bioVisible: false},
         {name: "Tibor", bio: "Incididunt photo booth ethical reprehenderit adipisicing", bioVisible: false},
         {name: "Ziva", bio: "Excepteur shabby chic semiotics Marfa", bioVisible: false}
-      ]
+      ],
+      newPersonName: "",
+      newPersonBio: ""
     },
     methods: {
       toggleBio: function(inputPerson) {
         inputPerson.bioVisible = !inputPerson.bioVisible;
+      },
+      addPerson: function() {
+        this.people.push({name: this.newPersonName, bio: this.newPersonBio, bioVisible: false});
+        this.newPersonName = "";
+        this.newPersonBio = "";
       }
     }
   });

52382d81cb008aadfe75b765d6423ed89667f139
Peter Jang <peter.x.jang@gmail.com>
2017-04-24 19:47:35 -0500

    Show people using v-for

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -3,7 +3,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      people: [
+        {name: "Bob", bio: "Small batch salvia Marfa chillwave delectus", bioVisible: false},
+        {name: "Alice", bio: "Tattooed letterpress gluten-free ugh", bioVisible: false},
+        {name: "Tibor", bio: "Incididunt photo booth ethical reprehenderit adipisicing", bioVisible: false},
+        {name: "Ziva", bio: "Excepteur shabby chic semiotics Marfa", bioVisible: false}
+      ]
     }
   });
 });

a9f676bf7c5b46d1d96f103d439d30e669a8fc29
Peter Jang <peter.x.jang@gmail.com>
2017-04-25 19:50:14 -0500

    Get data from api using $.get

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -1,16 +1,16 @@
-/* global Vue */
+/* global Vue, $ */
 document.addEventListener("DOMContentLoaded", function(event) { 
   var app = new Vue({
     el: '#app',
     data: {
-      people: [
-        {name: "Bob", bio: "Small batch salvia Marfa chillwave delectus", bioVisible: false},
-        {name: "Alice", bio: "Tattooed letterpress gluten-free ugh", bioVisible: false},
-        {name: "Tibor", bio: "Incididunt photo booth ethical reprehenderit adipisicing", bioVisible: false},
-        {name: "Ziva", bio: "Excepteur shabby chic semiotics Marfa", bioVisible: false}
-      ],
+      people: [],
       newPersonName: "",
       newPersonBio: ""
+    },
+    mounted: function() {
+      $.get("/api/v1/people", function(responseData) {
+        this.people = responseData;
+      }.bind(this));
     },
     methods: {
       toggleBio: function(inputPerson) {
